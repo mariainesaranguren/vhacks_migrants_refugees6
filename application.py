607fe02b9b38b5d464b3eed0887117f2a9362255
Anushree Agrawal <anushree.agrawal27@gmail.com>
2018-03-09 17:26:28 +0100

    add userinfo

--- application.py
+++ application.py
@@ -14,6 +14,15 @@ def login():
 def roles():
 	return render_template("role.html")
 
-@app.route("/skills")
+@app.route("/skills", methods=["GET", "POST"])
 def skills():
 	return render_template("skills.html")
+
+@app.route("/user_info", methods=["GET", "POST"])
+def user_info():
+	if request.method == 'POST':
+		print 'post form'
+		print request.values.get('location')
+	else:
+		return render_template("user_info.html")
+
